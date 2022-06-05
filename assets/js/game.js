7e338e11f7b46b1f17938513c31e744bb6d41aef
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-06-04 20:47:15 -0500

    added fight function loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,13 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth, playerName + " has " + playerMoney + " dollars");
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+
+
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     //prompt fight or no-fight option
@@ -63,4 +63,6 @@ var fight = function () {
         window.alert("You need to choose a valid option. Try again!");
     }
 }
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
